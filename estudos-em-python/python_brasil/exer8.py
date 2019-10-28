4ded8217f914d147e6e2f5f3e4e3443525de86ae
João Pedro Zanetti Gonçalves <42424888+joaopedrozg@users.noreply.github.com>
2019-10-28 16:29:21 -0300

    Add files via upload

---
+++ estudos-em-python/python_brasil/exer8.py
@@ -0,0 +1,7 @@
+print('cálculo salarial mensal conforme horas trabalhadas')
+valor_hora = input('quantos você ganha por hora? ')
+quant_horas_trabalhadas = input('quantas horas você trabalhou este mês? ')
+salario = float(valor_hora) * float(quant_horas_trabalhadas)
+
+print('seu salário foi de : ' + str(salario) + 'R$')
+
---
+++ estudos-em-python/python_brasil/exer9.py
@@ -0,0 +1,4 @@
+print('conversor de graus Farenheit em Celsius')
+farenheit = input('quantidade de graus Farenheit: ')
+celsius = 5 * (float(farenheit) - 32)/9
+print(str(farenheit) + ' convertido em celsius é : ' + str(celsius))
